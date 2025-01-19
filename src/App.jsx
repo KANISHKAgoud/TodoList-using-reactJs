b1715ec575c1e3926fdb9b0cffe90e4434c3c998
kuki <goudkanishka@gmail.com>
2025-01-19 19:21:01 +0530

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,38 @@ import viteLogo from '/vite.svg'
 import Navbar from './components/navbar'
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [todo, settodo] = useState("")
   const [alltodos, setAlltodos] = useState([])
   const [Editindex, setEditindex] = useState(null)
-
+  
   let handleChange = (e) => {
     settodo(e.target.value)
   }
 
   let handledelete = (index) => {
-    setAlltodos(alltodos.filter((e,i) => {
+    setAlltodos(alltodos.filter((_, i) =>
       i !== index
-    }))
+    ))
   }
   let handleedit = (index) => {
     settodo(alltodos[index])
     setEditindex(index)
   }
 
+  let handletoggle = (index) => {
+    let updated = [...alltodos]
+      updated[index].completed = !updated[index].completed
+      setAlltodos(updated)
+  }
+
+
   let handleadd = () => {
     let updated = [...alltodos]
-    if (Editindex === null)
-    {
-    updated.push(todo)
+    if (Editindex === null) {
+      updated.push({text:todo, completed:false})
     }
-    else{
-      updated[Editindex] = todo
+    else {
+      updated[Editindex] = {text:todo}
     }
     setAlltodos(updated)
     setEditindex(null)
@@ -43,29 +48,38 @@ function App() {
 
   return (
     <>
-      <div className='mx-auto bg-green-50'>
+      <div className='bg-green-50 w-full h-full'>
         <Navbar />
-        <div className='bg-green-100 w-3/4 mx-auto my-4 p-4 rounded-xl'>
-          <div className='flex gap-x-8 font-bold text-xl justify-center'>
-            <li className='list-none'>Add todo</li>
-            <input className='rounded-2xl' type="text" value={todo} onChange={handleChange} />
-            <button className='bg-green-600 h-10 w-24 rounded-md cursor-pointer hover:bg-green-800' onClick={handleadd}>
-              {Editindex === null ? "ADD" : "UPDATE"}
-            </button>
+        <div className='bg-green-100 h-full w-11/12 md:w-3/4 mx-auto my-4 p-4 rounded-xl '>
+          <div className='md:flex gap-x-8 font-bold text-xl justify-center'>
+            <li className='list-none whitespace-nowrap mb-1'>Add todo</li>
+            <div className='flex'>
+
+              <input className='rounded-2xl w-3/4 sm:w-4/6 mr-4' type="text" value={todo} onChange={handleChange} />
+              <button className='bg-green-600 h-10 w-16 sm:w-24 rounded-md cursor-pointer hover:bg-green-800' onClick={handleadd}>
+                {Editindex === null ? "ADD" : "UPDATE"}
+              </button>
+            </div>
           </div>
-          <div className='font-bold text-2xl'>Your Todos</div>
+          <div className='font-bold text-2xl mt-12'>Your Todos</div>
 
-          {alltodos.map((e,index) => {
+          {alltodos.map((e, index) => {
             return (<div key={index} className='relative text-lg bg-green-50 flex gap-x-7 my-8'>
-
-              <div className=' w-1/4 rounded-lg h-8 pl-3'>{e}</div>
+              <div className='' onClick={() => {
+                handletoggle(index)
+              }}>
+                {e.completed ? (<i className="fa-solid fa-circle-check"></i>): (<i className="fa-regular fa-circle"></i>)}
+                </div>
+              <div style={{textDecoration: e.completed ? "line-through": "none"}} className=' w-1/4 rounded-lg h-8 pl-3'>
+              {e.text}
+              </div>
               <div className='absolute flex gap-x-9 right-4'>
-              <button className='' onClick={() => handleedit(index)}>
-                <i className="fa-solid fa-pen-to-square"></i>
-              </button>
-              <button className='' onClick={() => handledelete(index)}>
-                <i className="fa-solid fa-trash-can"></i>
-              </button>
+                <button className='' onClick={() => handleedit(index)}>
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button className='' onClick={() => handledelete(index)}>
+                  <i className="fa-solid fa-trash-can"></i>
+                </button>
               </div>
             </div>
             )
